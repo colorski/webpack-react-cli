c2301676a189a40f7ea616af0a0bab9d37197e1a
colorski <290518066@qq.com>
2019-03-05 17:28:50 +0800

    resove

--- src/components/TextView.js
+++ src/components/TextView.js
@@ -14,7 +14,7 @@ export default class TextView extends Component {
       <p>text: { text }</p>
       <p>sum: { Math.ceil(sum) }</p>
       <p>data: { d.say }</p>
-      <p>event: <span onClick={()=>alert('蟹蟹！')}>{ d.btnText }</span></p>
+      <p>event: <span onClick={()=>alert('蟹蟹！111')}>{ d.btnText }</span></p>
       <hr />
       <h2>url-loader</h2>
       <p>大于1kb的图片：<img src={imgOut1} /></p>
